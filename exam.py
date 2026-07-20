7811fafa904cdb91f4696be81bc97408f699f8c0
Sharif123098 <msm4567work@gmail.com>
2026-07-20 23:15:34 +0545

    Delete exam.py

--- exam.py
+++
@@ -1,7 +0,0 @@
-def average_score(*marks):
-    total = sum(marks)
-    average = total / len(marks)
-    return average
-
-print(average_score(85, 90, 78, 92, 88))
-print(average_score(75, 80, 70))
